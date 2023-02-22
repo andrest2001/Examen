ab087cf6679ba0313ada47e4dfcec8d255024f87
Andrés Toledano <andres13072001@gmail.com>
2023-02-22 12:48:48 +0100

    Terminado el boton de eliminar y el boton de editar con sus respectivos metodos.

--- routes/images.js
+++ routes/images.js
@@ -23,7 +23,34 @@ router.post('/add',async(req,res)=>{
     }
     
     await pool.query('INSERT INTO images SET ?', [newImage])
+    res.redirect('/images')
 })
 
+router.get('/delete/:id' , async(req,res)=>{
+    const {id}= req.params
+    await pool.query('DELETE  FROM images WHERE id = ?', [id])
+    
+    
+    res.redirect('/images')
+})
 
+router.get('/edit/:id', async(req,res)=>{
+    const {id}= req.params
+    const [images] = await pool.query('SELECT * FROM images WHERE id = ?', [id])
+    console.log(images)
+    res.render('images/edit', {images:images[0]})
+    res.redirect('/images')
+})
+
+router.post('/edit/:id', async(req,res)=>{
+    const { id } = req.params
+    const {titulo, imagen, descripcion}= req.body
+    const newImage ={
+        titulo,
+        imagen,
+        descripcion
+    }
+    await pool.query('UPDATE images SET ? WHERE id = ?',[newImage, id])
+    res.redirect('/images')
+})
 module.exports = router;
